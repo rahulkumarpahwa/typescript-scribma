8cf2706e12932d73698a1000b9e875e2c0c75b42
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2026-07-01 22:53:55 +0530

    L51 Completed

--- TypeScript in Express/Challenge.md
+++ TypeScript in Express/Challenge.md
@@ -17,4 +17,10 @@
     1. Use the `Express` type where it makes sense
     2. Add a `void` return type where it makes sense
 
-4. 
+4. CHALLENGE: Send back a response
+    1. Add a GET route for `/` that responds with an empty JSON object.
+    2. Compile the app with `npx tsc`
+    3. Run the compiled JavaScript with `node dist/index.js`
+    (Ignore any TypeScript warning for now)
+
+5. 
--- TypeScript in Express/src/index.ts
+++ TypeScript in Express/src/index.ts
@@ -1,25 +1,33 @@
 /*
-CHALLENGE: Build a basic Express app using vanilla JavaScript
-1. Import Express and create an app instance.
-2. Listen on port 8000.
-3. When the server starts, log: "Listening on port 8000"
+CHALLENGE: Send back a response
+1. Add a GET route for `/` that responds with an empty JSON object.
+2. Compile the app with `npx tsc`
+3. Run the compiled JavaScript with `node dist/index.js`
    
-You won't need to create any endpoints or routes yet
-*/
-
-/*
-MINI CHALLENGE: Add some types  
-1. Use the `Express` type where it makes sense
-2. Add a `void` return type where it makes sense
+(Ignore any TypeScript warning for now)
 */
 
 import express from "express";
-import type { Express } from "express";
-
+import type { Express, Request, Response } from "express";
+import { env as loadEnv } from "custom-env";
 const app: Express = express();
+
+loadEnv();
+
+app.get("/", (req : Request, res : Response) =>{
+    res.status(200).json({})
+})
+
+
+
+
+
+
+
 
 const server = app.listen(process.env.PORT, (): void => {
     console.log(`Listening on port ${process.env.PORT}`)
+    console.log(`http://localhost:${process.env.PORT}`)
 })
 
 server.setTimeout(50000);
